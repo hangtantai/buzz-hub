a644c2bff6a11bed26edfbfb80a242d0295c3e1f
hangtantai <htt31124@gmail.com>
2024-04-28 11:54:28 +0700

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'dart:io';
+import 'package:buzz_hub/modules/search/views/search_bar.dart';
 import 'package:buzz_hub/modules/login/views/conversation_page.dart';
+import 'package:buzz_hub/modules/profile/views/profile.dart';
 import 'package:buzz_hub/modules/login/views/login_page.dart';
 import 'package:buzz_hub/modules/login/views/register_page.dart';
 import 'package:buzz_hub/services/auth_service.dart';
 import 'package:buzz_hub/services/dto/requests/login_request.dart';
+import 'package:buzz_hub/modules/search/views/search_bar.dart' as BuzzHubSearchBar;
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+
 
 void main(List<String> arguments) async {
   HttpOverrides.global = MyHttpOverrides();
@@ -13,6 +18,24 @@ void main(List<String> arguments) async {
 
   runApp(const MyApp());
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         useMaterial3: true,
+//         fontFamily: 'Manrope',
+//       ),
+//       home: LoginPage(),
+//       // actions: [Searchbar()],
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -25,11 +48,31 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         fontFamily: 'Manrope',
       ),
-      home: LoginPage(),
+      home: MySearchBarApp(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         useMaterial3: true,
+//         fontFamily: 'Manrope',
+//       ),
+//       home: ProfileScreen(),
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// }
+
+
+
 
 class MyHttpOverrides extends HttpOverrides {
   @override
