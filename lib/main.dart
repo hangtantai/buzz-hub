acc38d06cca9328b270ecfa77355501b86820ea0
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2024-05-19 15:08:00 +0700

    fix conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'dart:io';
 import 'package:buzz_hub/modules/search/views/search_bar.dart';
-import 'package:buzz_hub/modules/login/views/conversation_page.dart';
-import 'package:buzz_hub/modules/profile/views/profile.dart';
-import 'package:buzz_hub/modules/login/views/login_page.dart';
-import 'package:buzz_hub/modules/login/views/register_page.dart';
 import 'package:buzz_hub/services/auth_service.dart';
 import 'package:buzz_hub/services/dto/requests/login_request.dart';
 import 'package:buzz_hub/modules/search/views/search_bar.dart' as BuzzHubSearchBar;
