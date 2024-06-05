3de132bd8f8a383c850f8748acb96534a3e8d26d
MinhChi <triedmlaunchpad@gmail.com>
2024-06-05 20:34:27 +0700

    [post + bookmark] UI

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Manrope',
       ),
       home: LoginPage(),
+      // home: CreatePost(),
       debugShowCheckedModeBanner: false,
     );
   }
